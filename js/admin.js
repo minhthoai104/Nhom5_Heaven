bc62762d4b8568138a60489e769b3055053f4bd9
qt1503 <quytien1503@email.com>
2024-11-24 18:21:58 +0700

    Quan ly CTKM

--- js/admin.js
+++ js/admin.js
@@ -33,6 +33,7 @@ let DATABASE = localStorage.getItem("DATABASE")
         },
       ],
       ORDERS: [],
+      PROMOTIONS: [],
     };
 
 localStorage.setItem("DATABASE", JSON.stringify(DATABASE));
@@ -41,6 +42,7 @@ localStorage.setItem("DATABASE", JSON.stringify(DATABASE));
 let PRODUCTS = DATABASE.PRODUCTS;
 let ACCOUNTS = DATABASE.ACCOUNTS;
 let ORDERS = DATABASE.ORDERS;
+let PROMOTIONS = DATABASE.PROMOTIONS;
 
 // ********************************* Generate DATA ********************************
 
@@ -51,6 +53,7 @@ function generateData() {
   // read json file
   $.getJSON("js/data.json", function (data) {
     DATABASE.PRODUCTS = data.PRODUCTS;
+    DATABASE.PROMOTIONS = data.PROMOTIONS;
     localStorage.setItem("DATABASE", JSON.stringify(DATABASE));
     location.reload();
   });
@@ -231,7 +234,7 @@ function notificationAction(content, color) {
 
   setTimeout(function () {
     notifi.style.display = "none";
-  }, 3000);
+  }, 2500);
 }
 
 // Delete And Update Product
@@ -363,25 +366,31 @@ function searchCompare(searchInput, productName) {
 let s_product = document.getElementById("s_product");
 let s_user = document.getElementById("s_user");
 let s_order = document.getElementById("s_order");
+let s_promotion = document.getElementById("s_promotion");
 
 let product_manager = document.getElementById("product-manager");
 let user_manager = document.getElementById("user-manager");
 let order_manager = document.getElementById("order-manager");
+let promo_manager = document.getElementById("promo-manager");
+
 
 s_product.addEventListener("click", showProductManager);
 s_user.addEventListener("click", showUserManager);
 s_order.addEventListener("click", showOrderManager);
+s_promotion.addEventListener("click", showPromoManager);
 
 function showProductManager() {
   product_manager.style.display = "block";
   user_manager.style.display = "none";
   order_manager.style.display = "none";
+  promo_manager.style.display = "none";
 }
 
 function showUserManager() {
   product_manager.style.display = "none";
   user_manager.style.display = "block";
   order_manager.style.display = "none";
+  promo_manager.style.display = "none";
   renderAccount();
 }
 
@@ -389,7 +398,16 @@ function showOrderManager() {
   product_manager.style.display = "none";
   user_manager.style.display = "none";
   order_manager.style.display = "block";
+  promo_manager.style.display = "none";
   renderOrder();
+}
+
+function showPromoManager() {
+  product_manager.style.display = "none";
+  user_manager.style.display = "none";
+  order_manager.style.display = "none";
+  promo_manager.style.display = "block";
+  renderPromo();
 }
 
 // ========================================= OPEN TAB MANAGER ===========================================
@@ -634,4 +652,315 @@ function actUpdateOrderStatus() {
       });
     });
   });
+}
+
+// ========================================= PROMOTION MANAGER ===========================================
+
+let promo_tbody = document.getElementById("promo_tbody");
+let promo_code = document.getElementById("promo_code");
+let classify = document.getElementById("classify");
+let name_promo = document.getElementById("name_promo");
+let description = document.getElementById("description");
+let condition = document.getElementById("condition");
+let amount_promo = document.getElementById("amount_promo");
+let start = document.getElementById("start");
+let end = document.getElementById("end");
+
+function renderPromo(){
+  promo_tbody.innerHTML = "";
+
+  PROMOTIONS.forEach((promo) => {
+    const row = document.createElement("tr");
+
+    const classifies = {
+      "0": "Phần Trăm",
+      "1": "Tiền",
+    };
+
+    let classifypromo = classifies[promo.classify];
+    let formattedAmount = promo.amount;
+    if (promo.classify === "1") {
+      formattedAmount = formatter.format(promo.amount);
+    }
+    else{
+      formattedAmount = promo.amount +"%";
+    }
+    row.innerHTML = `
+        <td>${promo.procode}</td>
+        <td>${promo.promotionName}</td>
+        <td>${classifypromo}</td>
+        <td>${formattedAmount}</td>
+        <td>${promo.promodes}</td>
+        <td>${formatter.format(promo.condition)}</td>
+        <td>${promo.begin}</td>
+        <td>${promo.end}</td>
+        <td class="text-center">
+            <i class="fas fa-trash-alt text-danger" data-code="${promo.procode}" id="delete-promo"></i>
+            <i class="fas fa-edit text-info" data-code="${promo.procode}" id="edit-promo"></i>
+            <i class="fas fa-info-circle text-success" data-code="${promo.procode}" id="detail-promotion" data-toggle="modal" data-target="#promoModal"></i>
+        </td>
+    `;
+
+    promo_tbody.appendChild(row);
+});
+}
+
+let notifi_promo = document.getElementById("notifi-promo");
+let notifi_content_pro = document.getElementById("notifi_content_promo");
+
+function notificationActionPM(content, color) {
+  notifi_content_pro.innerHTML = content;
+  notifi_promo.style.display = "block";
+  notifi_promo.style.width = "270px";
+  notifi_promo.style.backgroundColor = color;
+
+  setTimeout(function () {
+    notifi_promo.style.display = "none";
+  }, 2500);
+}
+
+let add_new_promo = document.getElementById("add_new_promo");
+add_new_promo.addEventListener("click", actAddPromo);
+
+function generatePromoCode() {
+  let itemCount = PROMOTIONS.length;
+  if(itemCount < 10){
+    return "CT0" + (itemCount + 1);
+  }else{
+    return "CT" + itemCount;
+  }
+  
+}
+
+function actAddPromo() {
+  let promoCode = generatePromoCode();
+  let promo = {
+    procode: promoCode,
+    promotionName: name_promo.value,
+    begin: start.value,
+    end: end.value,
+    promodes: description.value,
+    condition: parseInt(condition.value),
+    classify: parseInt(classify.value),
+    amount: parseInt(amount_promo.value),
+  };
+
+  if (validateFormPM(promo) === true) {
+    PROMOTIONS.push(promo);
+    localStorage.setItem("DATABASE", JSON.stringify(DATABASE));
+    renderPromo();
+    notificationActionPM("Thêm CTKM Thành Công.", "#12e64b");
+
+    let form_id_promo = document.getElementById("form_id_promo");
+    form_id_promo.querySelectorAll("input").forEach(function (input) {
+      input.value = "";
+      input.style.border = "1px solid #ced4da";
+    });
+  }
+  add_new_promo.disabled = false;
+}
+
+function validateFormPM(promo) {
+  if (  
+    promo.promotionName === "" ||  
+    promo.begin === "" ||  
+    promo.end === "" || parseInt(promo.condition) < 0 ||  
+    promo.promodes === "" || parseInt(promo.amount) <= 0 ||  
+    promo.condition === "" ||  
+    promo.classify === "" ||
+    promo.amount === "" || promo.begin >= promo.end ||
+    (promo.classify === 0 && promo.amount >= 90)
+  ) {  
+
+    let message_pro = "Vui Lòng Điền Đầy Đủ Thông Tin.";  
+    if (parseInt(promo.condition) < 0) {  
+      message_pro = "Điều kiện phải lớn hơn hoặc bằng 0.";  
+    } else if (parseInt(promo.amount) <= 0) {  
+      message_pro = "Giá trị giảm phải lớn hơn 0.";  
+    } else if(promo.begin >= promo.end){
+      message_pro = "Ngày kết thúc phải sau ngày bắt đầu."
+    }else if(promo.classify === 0 && promo.amount >= 90){
+      message_pro = "Không được giảm lớn hơn 90%.";  
+    }
+    
+    notificationActionPM(message_pro, "#e61212");  
+ 
+    let form_id_promo = document.getElementById("form_id_promo");  
+    let form_input_promo = form_id_promo.querySelectorAll("input");  
+
+    form_input_promo.forEach(function (input) {  
+      if (input.id !== "promo_code" &&   
+          (input.value === "" ||   
+          (input.id === "condition" && parseInt(input.value) <= 0) ||   
+          (input.id === "amount" && parseInt(input.value) <= 0))) {  
+        input.style.border = "1px solid red";
+      } else {  
+        input.style.border = "1px solid #ced4da";
+      }  
+    });  
+
+    add_new_promo.disabled = true;
+    return false;  
+  }  
+  return true;
+}
+
+promo_tbody.addEventListener("click", actPromo);
+
+function actPromo(event) {
+  let ev = event.target;
+  let data_code_promo = ev.getAttribute("data-code");
+  if (!ev) {return;}
+
+  // Edit
+  if (ev.matches("#edit-promo")) {
+    let update_promo = document.getElementById("update_promo");
+    let promoFilter = PROMOTIONS.filter(
+      (promo) => promo.procode === data_code_promo);
+
+    promo_code.value = promoFilter[0].procode;
+    classify.value = promoFilter[0].classify;
+    name_promo.value = promoFilter[0].promotionName;
+    description.value = promoFilter[0].promodes;
+    condition.value = promoFilter[0].condition;
+    amount_promo.value = promoFilter[0].amount;
+    start.value = promoFilter[0].begin;
+    end.value = promoFilter[0].end;
+
+    add_new_promo.style.display = "none";
+    update_promo.style.display = "inline-block";
+    promo_code.disabled = true;
+    document.documentElement.scrollTop = 0;
+  }
+  // Detail
+  if (ev.matches("#detail-promotion")) {
+    let promo_detail = document.getElementById("promo-detail");
+    let promoDetail = PROMOTIONS.filter(
+      (promo) => promo.procode === data_code_promo);
+
+    let formattedAmount = promoDetail[0].amount;
+    if (promoDetail[0].classify === "1") {
+      formattedAmount = formatter.format(promoDetail[0].amount);
+    }
+    else{
+      formattedAmount = promoDetail[0].amount +"%";
+    }
+    let modals_promo = `
+                <h6>${promoDetail[0].promotionName}</h6>
+                <p>Mã CTKM: ${promoDetail[0].procode} </p>
+                <p>Mô Tả: ${promoDetail[0].promodes}</p>
+                <p>Điều kiện: ${formatter.format(promoDetail[0].condition)}</p>
+                <p>Giá trị giảm: ${formattedAmount}</p>
+                <div>Ngày Bắt Đầu: ${promoDetail[0].begin}</div>
+                <div>Ngày Kết Thúc: ${promoDetail[0].end}</div>
+        `;
+        promo_detail.innerHTML = modals_promo;
+  }
+  // Delete
+  if (ev.matches("#delete-promo")) {
+    PROMOTIONS = PROMOTIONS.filter((promo) => promo.procode !== data_code_promo);
+    DATABASE.PROMOTIONS = PROMOTIONS;
+    localStorage.setItem("DATABASE", JSON.stringify(DATABASE));
+    ev.closest("tr").remove();
+    notificationActionPM("Xóa CTKM Thành Công.", "#38e867");
+  }
+}
+
+let update_promo = document.getElementById("update_promo");
+update_promo.addEventListener("click", actUpdatePromo);
+
+function actUpdatePromo() {
+  // Tìm sản phẩm cần cập nhật trong PRODUCTS bằng mã sản phẩm (code)
+  let promoToUpdate = PROMOTIONS.find((promo) => promo.procode === promo_code.value);
+
+  // Nếu không tìm thấy sản phẩm, dừng lại và thông báo lỗi
+  if (!promoToUpdate) {
+    notificationActionPM("Sản phẩm không tồn tại.", "#e83838");
+    return;
+  }
+
+  let updatedPromo = {
+    ...promoToUpdate, 
+    promotionName: name_promo.value,
+    begin: start.value,
+    end: end.value,
+    promodes: description.value,
+    condition: parseInt(condition.value),
+    classify: parseInt(classify.value),
+    amount: parseInt(amount_promo.value),
+  };
+  if (!validateForm(updatedPromo)) {
+    notificationActionPM("Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.", "#e83838");
+    return;
+  }
+  Object.assign(promoToUpdate, updatedPromo);
+  localStorage.setItem("DATABASE", JSON.stringify(DATABASE));
+  renderPromo();
+  notificationActionPM("Cập Nhật CTKM Thành Công.", "#1216e6");
+  resetFormPM();
+  update_promo.style.display = "none";
+  add_new_promo.style.display = "inline-block";
+  promo_code.disabled = false;
+}
+function resetForm() {
+  document.getElementById("form_id_promo").reset();
+  document.querySelectorAll("input").forEach((input) => {
+    input.style.border = "1px solid #ced4da";
+  });
+}
+
+let search_promo = document.getElementById("search_promo");
+search_promo.addEventListener("input", actSearchPM);
+
+function actSearchPM() {
+  let searchInput = search_promo.value;
+  let promoCompare = PROMOTIONS.filter((promo) =>
+    searchComparePM(searchInput, promo.promotionName)
+  );
+  promo_tbody.innerHTML = "";
+  promoCompare.forEach((promo) => {
+    renderPM(promo);
+  });
+}
+
+// Search Compare
+function searchComparePM(searchInput, proName) {
+  let searchInputLower = searchInput.toLowerCase();
+  let promoNameLower = proName.toLowerCase();
+  return promoNameLower.includes(searchInputLower);
+}
+
+function renderPM(promo) {
+  const row = document.createElement("tr");
+  const classifies = {
+    "0": "Phần Trăm",
+    "1": "Tiền",
+  };
+
+  let classifypromo = classifies[promo.classify];
+  let formattedAmount = promo.amount;
+  if (promo.classify === "1") {
+    formattedAmount = formatter.format(promo.amount);
+  }
+  else{
+    formattedAmount = promo.amount +"%";
+  }
+
+  row.innerHTML = `
+        <td>${promo.procode}</td>
+        <td>${promo.promotionName}</td>
+        <td>${classifypromo}</td>
+        <td>${formattedAmount}</td>
+        <td>${promo.promodes}</td>
+        <td>${formatter.format(promo.condition)}</td>
+        <td>${promo.begin}</td>
+        <td>${promo.end}</td>
+        <td class="text-center">
+            <i class="fas fa-trash-alt text-danger" data-code="${promo.procode}" id="delete-promo"></i>
+            <i class="fas fa-edit text-info" data-code="${promo.procode}" id="edit-promo"></i>
+            <i class="fas fa-info-circle text-success" data-code="${promo.procode}" id="detail-promotion" data-toggle="modal" data-target="#promoModal"></i>
+        </td>
+    `;
+
+    promo_tbody.appendChild(row);
 }
